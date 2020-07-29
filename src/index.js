590f8ed5e06d54c74ffde6ae73891f4db559b224
denzakh <den.zakh@gmail.com>
2020-07-29 17:37:50 +0300

    тестирование с redux

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {reducer, initialState} from "./reducer";
 const store = createStore(
   reducer,
   initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
 );
 
 const rootElement = document.getElementById(`root`);
